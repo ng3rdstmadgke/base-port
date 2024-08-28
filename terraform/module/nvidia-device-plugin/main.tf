5dd30142fe2288a68bc5a4acc3d6605ccba02fbf
ng3rdstmadgke <ng3rdstmadgke@gmail.com>
2024-08-28 21:41:33 +0900

    helmでnvidia-device-pluginを導入, karpenterで起動したGPUノードでpodを起動することに成功

--- terraform/module/nvidia-device-plugin/main.tf
+++ terraform/module/nvidia-device-plugin/main.tf
@@ -1,29 +1,10 @@
 /**
  * NVIDIA Device PluginをHelmでインストール
  * NVIDIA/k8s-device-plugin | GitHub: https://github.com/NVIDIA/k8s-device-plugin
- *
- * NOTE: helmでインストールするとうまくいかないので、AWS公式のやり方でインストール
- *       https://docs.aws.amazon.com/ja_jp/eks/latest/userguide/eks-optimized-ami.html#gpu-ami
+ *   helmでインストール: https://github.com/NVIDIA/k8s-device-plugin/tree/v0.16.2?tab=readme-ov-file#deployment-via-helm
  */
 
-resource "null_resource" "nvidia_device_plugin" {
-  triggers = {
-    # VERSION=0.16.2
-    # wget https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/v${VERSION}/deployments/static/nvidia-device-plugin.yml
-    file_hash = filebase64sha256("${path.module}/nvidia-device-plugin.yml")
-  }
-  
-  provisioner "local-exec" {
-    command = "kubectl apply -f ${path.module}/nvidia-device-plugin.yml > nvidia-device-plugin-install.log 2>&1"
-  }
-
-  provisioner "local-exec" {
-    command = "kubectl delete -n -f ${path.module}/nvidia-device-plugin.yml > nvidia-device-plugin-install.log 2>&1"
-    when    = destroy
-  }
-}
-
-/*
+//helm_release: https://registry.terraform.io/providers/hashicorp/helm/latest/docs/resources/release
 resource "helm_release" "nvidia_device_plugin" {
   repository = "https://nvidia.github.io/k8s-device-plugin"
   name       = "nvdp"
@@ -33,12 +14,8 @@ resource "helm_release" "nvidia_device_plugin" {
   namespace  = "nvidia-device-plugin"
   create_namespace = true
 
-  set {
-    // NVIDIA GPU Feature Discoveryを有効にする
-    // 有効化することで、NodeにGPUの情報を自動的にラベル付けし有効化する
-    // https://github.com/NVIDIA/k8s-device-plugin?tab=readme-ov-file#deploying-with-gpu-feature-discovery-for-automatic-node-labels
-    name  = "gfd.enabled"
-    value = true
-  }
+  // valuesのひな型: https://github.com/NVIDIA/k8s-device-plugin/blob/v0.16.2/deployments/helm/nvidia-device-plugin/values.yaml
+  values = [
+    file("${path.module}/values.yaml"),
+  ]
 }
-*/
