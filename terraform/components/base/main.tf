bf86a82dc12cca5edb3cc6732d1b98e45f03a1fa
ng3rdstmadgke <ng3rdstmadgke@gmail.com>
2025-03-03 22:30:10 +0900

    baseコンポーネント

---
+++ terraform/components/base/main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_version = "~> 1.10.3"
+
+  backend "s3" {
+  }
+
+  required_providers {
+    // AWS Provider: https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.84.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+  default_tags {
+    tags = {
+      PROJECT = "BASEPORT_PRD",
+    }
+  }
+}
+
+
+locals {
+  app_name = "baseport"
+}
+
+variable stage {
+  type = string
+  description = "ステージ名"
+}
+
+variable tfstate_region {
+  type = string
+  description = "tfstateが保存されているリージョン"
+}
+
+variable tfstate_bucket {
+  type = string
+  description = "tfstateが保存されているS3バケット"
+}
+
+
+output "app_name" {
+  value = local.app_name
+}
+
+output "cluster_name" {
+  value = "${local.app_name}-${var.stage}"
+}
+
+output "project_dir" {
+  value = abspath("${path.module}/../../..")
+}
